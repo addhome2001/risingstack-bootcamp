4f1cf8a46c9061febccbc16d7d709034a2656d2b
Seven <seven.chi@shopback.com>
2020-10-10 16:40:03 +0800

    Step 1: solution

--- web/config.js
+++ web/config.js
@@ -1,7 +1,27 @@
 'use strict'
 
+const Joi = require('joi')
+const logger = require('winston')
+
+const envVar = process.env
+const envSchema = Joi.object({
+  PORT: Joi
+    .number()
+    .integer()
+    .min(3000)
+    .max(3999)
+    .required()
+}).unknown()
+
+try {
+  Joi.attempt(envVar, envSchema)
+} catch (e) {
+  logger.error('There are some validation errors', e.details)
+  process.exit(1)
+}
+
 const config = {
-  port: 0
+  port: envVar.PORT,
 }
 
 module.exports = config
